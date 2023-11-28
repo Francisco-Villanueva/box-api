46069e777318238aee5b8c7c1a9168ef9d1077c0
Dario Beratti <darioberatti@gmail.com>
2023-11-28 16:39:55 -0300

    feat(register): auth.controller and auth.service edited. CORS configured.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { LoginAuthDto } from './dto/login-auth.dto'
+import { RegisterAuthDto } from './dto/register-auth.dto'
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,10 @@ export class AuthController {
 	@Post('login')
 	loginUser(@Body() userObjectLogin: LoginAuthDto) {
 		return this.authService.login(userObjectLogin)
+	}
+
+	@Post('register')
+	registerUser(@Body() userObjectRegister: RegisterAuthDto) {
+		return this.authService.register(userObjectRegister)
 	}
 }
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { checkEnvVariables } from 'config/envCheck'
 import * as morgan from 'morgan'
+import { CORS } from './constants'
 
 async function bootstrap() {
 	await checkEnvVariables()
 
 	const app = await NestFactory.create(AppModule)
 
+	app.enableCors(CORS)
 	app.use(morgan('tiny'))
 	app.setGlobalPrefix('api')
 
