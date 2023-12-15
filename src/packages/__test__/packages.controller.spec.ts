d4edec9e696d16e1d918c4d45aeb7e2389dba050
Mateo <mateo.navarro98@gmail.com>
2023-12-15 17:22:53 -0300

    feat:Packages services and controllers testing

--- src/packages/__test__/packages.controller.spec.ts
+++ src/packages/__test__/packages.controller.spec.ts
@@ -1,38 +1,122 @@
 import { Test, TestingModule } from '@nestjs/testing'
-import { PackagesController } from '../packages.controller'
 import { PackagesService } from '../packages.service'
-import { MongooseModule } from '@nestjs/mongoose'
-import { Package, PackageSchema } from '../schema/packages.schema'
+import { PackagesController } from '../packages.controller'
+import { PackageDto } from '../dto/package.dto'
 
 describe('PackagesController', () => {
-	let controller: PackagesController
+	let packageService: PackagesService
+	let packageController: PackagesController
+
+	const mockPackageObject = {
+		_id: '6571e920c5cfa9c54f6149d6',
+		address: 'Carlos Pellegrini 255, Buenos Aires, Argentina',
+		clientName: 'Juan Rodriguez',
+		weight: 5,
+		deliverDate: '2023-12-14',
+		status: 'NO ASIGNADO',
+	}
+
+	const mockPackage = [
+		{
+			_id: '6571e920c5cfa9c54f6149d6',
+			address: 'Carlos Pellegrini 255, Buenos Aires, Argentina',
+			clientName: 'Juan Rodriguez',
+			weight: 5,
+			deliverDate: '2023-12-14',
+			status: 'NO ASIGNADO',
+		},
+		{
+			_id: '2671e920c5cfa9c54f6149f0',
+			address: 'Carlos Pellegrini 500, Buenos Aires, Argentina',
+			clientName: 'Pedro Gonzalez',
+			weight: 2,
+			deliverDate: '2023-12-13',
+			status: 'PENDIENTE',
+		},
+		{
+			_id: '9071e920c5cfa9c54f6149p3',
+			address: 'Carlos Pellegrini 800, Buenos Aires, Argentina',
+			clientName: 'Juan Moralez',
+			weight: 3,
+			deliverDate: '2023-12-13',
+			status: 'PENDIENTE',
+		},
+	]
+
+	const mockPackageService = {
+		findAll: jest.fn().mockResolvedValueOnce(mockPackage),
+		create: jest.fn(),
+		findByID: jest.fn().mockResolvedValueOnce(mockPackageObject),
+		update: jest.fn(),
+	}
 
 	beforeEach(async () => {
 		const app: TestingModule = await Test.createTestingModule({
-			imports: [
-				MongooseModule.forRoot(process.env.MONGO_URI),
-				MongooseModule.forFeature([
-					{
-						name: Package.name,
-						schema: PackageSchema,
-					},
-				]),
-			],
 			controllers: [PackagesController],
-			providers: [PackagesService],
+			providers: [
+				{
+					provide: PackagesService,
+					useValue: mockPackageService,
+				},
+			],
 		}).compile()
 
-		controller = app.get<PackagesController>(PackagesController)
+		packageService = app.get<PackagesService>(PackagesService)
+		packageController = app.get<PackagesController>(PackagesController)
 	})
 
-	describe('getHello', () => {
-		it('should return "Hello Packages"', () => {
-			expect(controller.sayHello()).toBe('Hello Packages')
+	it('should be defined', async () => {
+		expect(packageController).toBeDefined()
+	})
+
+	describe('getAllPackages', () => {
+		it('should get all packages', async () => {
+			const result = await packageController.findAll()
+			expect(packageService.findAll).toHaveBeenCalled()
+			expect(result).toEqual(mockPackage)
 		})
 	})
-	// describe('getPackages', () => {
-	// 	it('should return all the packages', () => {
-	// 		expect(controller.findAll()).toHaveLength(14)
-	// 	})
-	// })
+
+	describe('createAPackage', () => {
+		it('should create a package', async () => {
+			const newPackage = {
+				_id: '6571e920c5cfa9c54f6149d6',
+				address: 'Carlos Pellegrini 255, Buenos Aires, Argentina',
+				clientName: 'Juan Rodriguez',
+				weight: 5,
+				deliverDate: '2023-12-14',
+				status: 'NO ASIGNADO',
+			}
+
+			mockPackageService.create = jest.fn().mockResolvedValueOnce(mockPackage[0])
+
+			const result = await packageController.create(newPackage as PackageDto)
+			expect(packageService.create).toHaveBeenCalled()
+			expect(result).toEqual(mockPackage[0])
+		})
+	})
+
+	describe('getPackageById', () => {
+		it('should get a package by ID', async () => {
+			const result = await packageController.findBy(mockPackageObject._id)
+			expect(packageService.findByID).toHaveBeenCalled()
+			expect(result).toEqual(mockPackageObject)
+		})
+	})
+
+	describe('updatePackage', () => {
+		it('should update a package', async () => {
+			const updatedPackage = { ...mockPackageObject, status: 'PENDIENTE' }
+			const newPackage = { ...mockPackageObject, status: 'PENDIENTE' }
+
+			mockPackageService.update = jest.fn().mockResolvedValueOnce(updatedPackage)
+
+			const result = await packageController.update(
+				mockPackageObject._id,
+				newPackage
+			)
+			expect(packageService.update).toHaveBeenCalled()
+			expect(result).toEqual(updatedPackage)
+		})
+	})
 })
--- src/packages/packages.controller.ts
+++ src/packages/packages.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Param, Put } from '@nestjs/common'
+import {
+	Controller,
+	Get,
+	Post,
+	Body,
+	Param,
+	Put,
+	HttpCode,
+	HttpStatus,
+} from '@nestjs/common'
 import { PackagesService } from './packages.service'
 import { PackageDto } from './dto/package.dto'
 import { UpdatePackageDto } from './dto/update-package.dto'
@@ -8,29 +17,29 @@ import { PackageStatus } from './constants'
 export class PackagesController {
 	constructor(private packageService: PackagesService) {}
 
-	@Get('/test')
-	sayHello() {
-		return 'Hello Packages'
-	}
-
 	@Get()
+	@HttpCode(HttpStatus.OK)
 	findAll() {
 		return this.packageService.findAll()
 	}
 	@Get(':id')
+	@HttpCode(HttpStatus.OK)
 	findBy(@Param('id') id: string) {
 		return this.packageService.findByID(id)
 	}
 	@Get('/status/:status')
+	@HttpCode(HttpStatus.OK)
 	findByStatus(@Param('status') status: PackageStatus) {
 		return this.packageService.findByStatus(status)
 	}
 
 	@Post()
+	@HttpCode(HttpStatus.CREATED)
 	create(@Body() createPackageDto: PackageDto) {
 		return this.packageService.create(createPackageDto)
 	}
 	@Put(':id')
+	@HttpCode(HttpStatus.CREATED)
 	update(@Param('id') id: string, @Body() data: UpdatePackageDto) {
 		return this.packageService.update(id, data)
 	}
